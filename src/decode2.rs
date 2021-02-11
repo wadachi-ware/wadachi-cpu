689bba6b4909c55541e216c3fd03ec3ad4727d04
Yasuda5160 <58543759+Yasuda5160@users.noreply.github.com>
2021-02-12 00:49:55 +0900

    Create decode2.rs

---
+++ src/decode2.rs
@@ -0,0 +1,158 @@
+use bit_field::BitField;
+use std::ops::Range;
+
+const OPCODE_RANGE: Range<usize> = 0..7;
+const RD_RANGE: Range<usize> = 7..12;
+const RS1_RANGE: Range<usize> = 15..20;
+const IMM_RANGE: Range<usize> = 20..32;
+const FUNCT3_RANGE: Range<usize> = 12..15;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Instruction {
+    Addi(IType),
+    Slli(IType),
+    Slti(IType),
+    Sltiu(IType),
+    Xori(IType),
+    Srli(IType),
+    Srai(IType),
+    Ori(IType),
+    Andi(IType),
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct IType {
+    rd: u8,
+    rs1: u8,
+    imm: u8,
+}
+
+impl IType {
+    fn new(instruction: u32) -> Self {
+        Self {
+            rd: instruction.get_bits(RD_RANGE) as u8,
+            rs1: instruction.get_bits(RS1_RANGE) as u8,
+            imm: instruction.get_bits(IMM_RANGE) as u8,
+        }
+    }
+}
+
+pub fn decode(instruction: u32) -> Instruction {
+    match instruction.get_bits(OPCODE_RANGE) {
+        // I Type
+        0b0010011 => match instruction.get_bits(FUNCT3_RANGE) {
+            0b000 => Instruction::Addi(IType::new(instruction)),
+            0b001 => Instruction::Slli(IType::new(instruction)),
+            0b010 => Instruction::Slti(IType::new(instruction)),
+            0b011 => Instruction::Sltiu(IType::new(instruction)),
+            0b100 => Instruction::Xori(IType::new(instruction)),
+            0b101 => match instruction.get_bits(IMM) {
+                0b0000000_00000=>Instruction::Srli(IType::new(instruction)),
+                0b0100000_00000=>Instruction::Srai(IType::new(instruction)),
+                _ => unimplemented!(),
+            },
+            0b110 => Instruction::Ori(IType::new(instruction)),
+            0b111 => Instruction::Andi(IType::new(instruction)),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn decode_rv32i_r() {
+        // addi x1, x9, 64
+        assert_eq!(
+            Instruction::Addi(IType {
+                rd: 1,
+                rs1: 9,
+                imm: 64,
+            }),
+            decode(0b000010_00000_01001_000_00001_0010011)
+        );
+
+        // slli x2, x6, 17
+        assert_eq!(
+            Instruction::Slli(IType {
+                rd: 2,
+                rs1: 6,
+                imm: 17,
+            }),
+            decode(0b000000_10001_00110_001_00010_0010011)
+        );
+
+        // slti x3, x4, 16
+        assert_eq!(
+            Instruction::Slti(IType {
+                rd: 3,
+                rs1: 4,
+                imm: 16,
+            }),
+            decode(0b0000000_10000_00100_010_00011_0010011)
+        );
+
+        // sltiu x4, x19, 8
+        assert_eq!(
+            Instruction::Sltiu(IType {
+                rd: 4,
+                rs1: 19,
+                imm: 8,
+            }),
+            decode(0b0000000_01000_10011_011_00100_0010011)
+        );
+
+        // xori x5, x12, 4
+        assert_eq!(
+            Instruction::Xori(IType {
+                rd: 5,
+                rs1: 12,
+                imm: 4,
+            }),
+            decode(0b0000000_00100_01100_100_00101_0010011)
+        );
+
+        // srli x6, x17, 0
+        assert_eq!(
+            Instruction::Srli(IType {
+                rd: 6,
+                rs1: 17,
+                imm: 0,
+            }),
+            decode(0b0000000_00000_10001_101_00110_0010011)
+        );
+
+        // srai x7, x27, 1024
+        assert_eq!(
+            Instruction::Srai(IType {
+                rd: 7,
+                rs1: 27,
+                imm: 1024,
+            }),
+            decode(0b0100000_00000_11011_101_00111_0010011)
+        );
+
+        // ori x8, x13, 2
+        assert_eq!(
+            Instruction::Ori(IType {
+                rd: 8,
+                rs1: 13,
+                imm: 2,
+            }),
+            decode(0b0000000_00010_01101_110_01000_0010011)
+        );
+
+        // andi x9, x30, 1
+        assert_eq!(
+            Instruction::Andi(IType {
+                rd: 9,
+                rs1: 30,
+                rs2: 1,
+            }),
+            decode(0b0000000_00001_11110_111_01001_0010011)
+        );
+    }
+}
