9017f0d81285db1dfe519fed1def5de806d43fe6
ikanago <28985004+ikanago@users.noreply.github.com>
2021-02-11 16:59:01 +0900

    feat: Decode `sub`

--- src/decode.rs
+++ src/decode.rs
@@ -1,8 +1,18 @@
 use bit_field::BitField;
+use std::ops::Range;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Instruction {
     Add(RType),
+    Sub(RType),
+    Sll(RType),
+    Slt(RType),
+    Sltu(RType),
+    Xor(RType),
+    Srl(RType),
+    Sra(RType),
+    Or(RType),
+    And(RType),
 }
 
 #[derive(Debug, PartialEq, Eq)]
@@ -12,18 +22,31 @@ pub struct RType {
     rs2: u8,
 }
 
-pub fn decode(instruction: u32) -> Instruction {
-    match instruction.get_bits(0..7) {
-        0b0110011 => {
-            match (instruction.get_bits(12..15), instruction.get_bits(25..32)) {
-                (0b000, 0b0000000) => Instruction::Add(RType {
-                    rd: instruction.get_bits(7..12) as u8,
-                    rs1: instruction.get_bits(15..20) as u8,
-                    rs2: instruction.get_bits(20..25) as u8,
-                }),
-                _ => unimplemented!(),
-            }
+impl RType {
+    fn new(instruction: u32) -> Self {
+        Self {
+            rd: instruction.get_bits(7..12) as u8,
+            rs1: instruction.get_bits(15..20) as u8,
+            rs2: instruction.get_bits(20..25) as u8,
         }
+    }
+}
+
+const OPCODE_RANGE: Range<usize> = 0..7;
+const FUNCT3_RANGE: Range<usize> = 25..32;
+const FUNCT7_RANGE: Range<usize> = 12..15;
+
+pub fn decode(instruction: u32) -> Instruction {
+    match instruction.get_bits(OPCODE_RANGE) {
+        // R Type
+        0b0110011 => match instruction.get_bits(FUNCT7_RANGE) {
+            0b000 => match instruction.get_bits(FUNCT3_RANGE) {
+                0b0000000 => Instruction::Add(RType::new(instruction)),
+                0b0100000 => Instruction::Sub(RType::new(instruction)),
+                _ => panic!("Invalid instruction"),
+            },
+            _ => unimplemented!(),
+        },
         _ => unimplemented!(),
     }
 }
@@ -43,6 +66,17 @@ mod tests {
                 rs2: 5,
             }),
             decode(add)
+        );
+
+        // sub x2, x6, x21
+        let sub = 0b0100000_10101_00110_000_00010_0110011;
+        assert_eq!(
+            Instruction::Sub(RType {
+                rd: 2,
+                rs1: 6,
+                rs2: 21,
+            }),
+            decode(sub)
         );
     }
 }
